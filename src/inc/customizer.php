bb4464faa377f62bbdfd2a08ac0d7c0cfa950f5f
Jakob Heinrich <reg@jakobheinrich.de>
2021-11-03 15:00:30 +0100

    typos

--- src/inc/customizer.php
+++ src/inc/customizer.php
@@ -1,10 +1,10 @@
 <?php
 function add_maintenence_mode($wp_customizer) {
     // add a setting for the site logo
-    $wp_customize->add_setting('work_in_progress');
+    $wp_customizer->add_setting('work_in_progress');
     // Add a control to upload the logo
-    $wp_customize->add_control( new WP_Customize_Control(
-        $wp_customize, 'work_in_progress', array(
+    $wp_customizer->add_control( new WP_Customize_Control(
+        $wp_customizer, 'work_in_progress', array(
             'label' => 'Show Work in Progress Page',
             'section' => 'title_tagline',
             'settings' => 'work_in_progress',
