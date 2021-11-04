0226df90827aaed0c3032f22052b31ee8c1f977b
Jakob Heinrich <reg@jakobheinrich.de>
2021-11-04 10:29:51 +0100

    wip page done

--- src/workinprogress.php
+++ src/workinprogress.php
@@ -1,3 +1,25 @@
-<?
-print('test');
-?>
+<!DOCTYPE html>
+<head>
+    <style media="screen">
+        body {
+            font-family: monospace;
+            font-size: 3em;
+            text-align: center;
+        }
+        div {
+            position: absolute;
+            top: 40%;
+            width: 100%
+
+        }
+
+
+
+    </style>
+</head>
+<body>
+    <div>
+        <h1>Das 'et cetera' der Intersektionalität</h1>
+        <h2>-- Website work in progress --</h2>
+    </div>
+</body>
