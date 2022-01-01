f366b0f3757aa4f84b80e72f2f80c8c0db20fcca
Jakob Heinrich <pn@jakobheinrich.de>
2022-01-01 16:22:18 +0100

    images exist

---
+++ src/page-home.php
@@ -0,0 +1,70 @@
+<?php /**
+* Template Name: Home-Page
+*
+* @package WordPress
+* @subpackage intersektionalitaet-etc.de
+* @since 1.0
+*/
+?>
+
+<?php get_header(); ?>
+
+<!-- header -->
+<header class="header clear" role="banner">
+
+		<!-- logo -->
+		<div class="logo-home">
+			<a href="<?php echo home_url(); ?>">
+				<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+				<img src="<?php echo get_template_directory_uri(); ?>/img/Logo.svg" alt="Logo" class="logo-img">
+			</a>
+		</div>
+		<!-- /logo -->
+
+
+</header>
+<!-- /header -->
+
+	<main role="main">
+		<!-- section -->
+		<section>
+
+			<h1><?php the_title(); ?></h1>
+
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+			<!-- article -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<?php the_content(); ?>
+
+				<?php comments_template( '', true ); // Remove if you don't want comments ?>
+
+				<br class="clear">
+
+				<?php edit_post_link(); ?>
+
+			</article>
+			<!-- /article -->
+
+		<?php endwhile; ?>
+
+		<?php else: ?>
+
+			<!-- article -->
+			<article>
+
+				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+
+			</article>
+			<!-- /article -->
+
+		<?php endif; ?>
+
+		</section>
+		<!-- /section -->
+	</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
